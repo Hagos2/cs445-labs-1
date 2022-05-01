c63862348feff6de3ff274b20a27d54acf7fe924
Gebrecherkos Hagos <gebrecherkoshagos@Gebrecherkoss-MacBook-Pro.local>
2022-05-01 03:08:47 -0500

    lab3 design pattern solutions

---
+++ typescript/src/lab2/ts-lab201.js
@@ -0,0 +1,23 @@
+// class University {
+//     constructor(name, dept) {
+//         this.name = name;
+//         this.dept = dept;
+//     }
+//     graduation(year) {
+//         console.log(`Graduating ${this.dept} ${year} students`);
+//     }
+// }
+// let miu = new University("MIU", "MSD");
+// miu.graduation(2021);
+var University = /** @class */ (function () {
+    function University(name, dept) {
+        this.name = name;
+        this.dept = dept;
+    }
+    University.prototype.graduation = function (year) {
+        console.log("Graduating ".concat(this.dept, " ").concat(year, " students"));
+    };
+    return University;
+}());
+var miu = new University("MIU", "MSD");
+miu.graduation(2021);
